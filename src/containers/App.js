baa8bb6a634744894867b2db4cad6653ab7e94f7
Rizchel Dayao <rdayao@us.ibm.com>
2017-03-13 18:12:59 -0500

    Changed color and font

--- src/containers/App.js
+++ src/containers/App.js
@@ -17,7 +17,7 @@ class App extends Component {
 
   retrieveMovies(callback) {
     this.props.retrieveMovies(this.state.movie);
-    setTimeout(() => {this.setState({ getMovies: true })}, 1000);
+    setTimeout(() => {this.setState({ getMovies: true })}, 800);
   }
 
   handleChange(e) {
@@ -30,10 +30,10 @@ class App extends Component {
   render() {
     return (
       <div className="container">
-        <h1>Movie Finder</h1>
+        <h1>Find That Movie</h1>
         <div className="set">
           <input type="text" className="input" placeholder="Enter movie..." value={this.state.movie} onChange={this.handleChange.bind(this)}/>
-          <button className="button" onClick={() => this.retrieveMovies()}>Find movie</button>
+          <button className="button" onClick={() => this.retrieveMovies()}>Search</button>
         </div>
         { this.state.getMovies ?
           <List movies={this.props.movies} />
