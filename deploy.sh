543cf729590903e6ed12ae7f0a61949e4b143f3d
Rizchel Dayao <rizchel.a.dayao@gmail.com>
2018-03-29 14:55:09 -0600

    Check ls

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
-echo $BXIAM
-./Bluemix_CLI/bin/bluemix login --apikey $BXIAM
+echo $(ls)
+# ./Bluemix_CLI/bin/bluemix login --apikey $BXIAM
 # export KUBECONFIG=$KUBECONFIG
 # kubectl delete all --all
 # bx cr build -t registry.ng.bluemix.net/rdayao/deploy-react-kubernetes .
