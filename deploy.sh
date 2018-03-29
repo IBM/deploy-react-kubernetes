4f72dfc2cb5834dddbd93f78cb863afc476a37ed
Rizchel Dayao <rizchel.a.dayao@gmail.com>
2018-03-29 14:30:11 -0600

    try to get script working

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
-./Bluemix_CLI/bin/bluemix --apikey $BXIAM
-export KUBECONFIG=$KUBECONFIG
-kubectl delete all --all
-bx cr build -t registry.ng.bluemix.net/rdayao/deploy-react-kubernetes .
-kubectl run deploy-react-kubernetes-deployment —-image registry.ng.bluemix.net/rdayao/deploy-react-kuberneteskubectl expose deployment/deploy-react-kubernetes-deployment
-—-type=NodePort —-name=deploy-react-kubernetes-service —-port=3000
+echo 'working'
+# ./Bluemix_CLI/bin/bluemix --apikey $BXIAM
+# export KUBECONFIG=$KUBECONFIG
+# kubectl delete all --all
+# bx cr build -t registry.ng.bluemix.net/rdayao/deploy-react-kubernetes .
+# kubectl run deploy-react-kubernetes-deployment —-image registry.ng.bluemix.net/rdayao/deploy-react-kuberneteskubectl expose deployment/deploy-react-kubernetes-deployment
+# —-type=NodePort —-name=deploy-react-kubernetes-service —-port=3000
