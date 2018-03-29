7420921a16f3120122f264fa88bf9ad1bbf44c71
Rizchel Dayao <rizchel.a.dayao@gmail.com>
2018-03-29 14:20:35 -0600

    Add deployment commands

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,6 @@
 ./Bluemix_CLI/bin/bluemix --apikey $BXIAM
-echo 'done'
+export KUBECONFIG=$KUBECONFIG
+kubectl delete all --all
+bx cr build -t registry.ng.bluemix.net/rdayao/deploy-react-kubernetes .
+kubectl run deploy-react-kubernetes-deployment —-image registry.ng.bluemix.net/rdayao/deploy-react-kuberneteskubectl expose deployment/deploy-react-kubernetes-deployment
+—-type=NodePort —-name=deploy-react-kubernetes-service —-port=3000
